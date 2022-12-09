919ec56b3a53f0450343f866d80ffaf57ed412b0
Muhammad Arif <71130457+ariefmuh@users.noreply.github.com>
2022-12-09 19:16:45 +0800

    Delete H071211004/Week6 directory

--- H071211004/Week6/Nomor3.sql
+++
@@ -1,8 +0,0 @@
--- Nomor 3
-SELECT UPPER(m.nama) nama_mahasiswa ,DAYNAME(tanggal) hari_seminar
-FROM ss_seminar s
-INNER JOIN ss_mahasiswa m
-ON s.id_mahasiswa = m.id_mahasiswa
-WHERE DAYNAME(tanggal) IN('Monday', 'Friday');
-SELECT * FROM ss_seminar;
-SELECT * FROM ss_mahasiswa;
